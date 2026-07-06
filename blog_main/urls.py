5937ca2f9b248688e0acf25c90ff2aabd7d723d4
farhadmonavar <farhad.monavar@gmail.com>
2026-07-06 21:00:42 +0330

    Template Inheritance, Context Processors and Post by Category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -24,4 +25,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('category/', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
