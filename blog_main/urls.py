31dca0ac8f9edfb26e063a30591b87fae0a1d41c
farhadmonavar <farhad.monavar@gmail.com>
2026-07-07 13:19:24 +0330

    Add search feature

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -20,12 +20,14 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from blogs import views as blog_views
+from blogs import views as BlogsViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/',blog_views.blogs, name='blogs'),
+    path('<slug:slug>/',BlogsViews.blogs, name='blogs'),
+    # Search endpoint
+    path('blogs/search', BlogsViews.search, name='search'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from matplotlib.pyplot import title
+
 from blogs.models import Blog, Category
+from django.db.models import Q
 
 
 def posts_by_category(request, category_id):
@@ -28,3 +31,16 @@ def blogs(request, slug):
         'single_blog': single_blog,
     }
     return render(request, 'blogs.html', context)
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword),
+                                Q(blog_body__icontains=keyword),
+                                status='Published')
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
